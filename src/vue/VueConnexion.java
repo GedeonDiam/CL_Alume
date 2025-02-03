e7012e69e19695aa2fd9698b0dc26c5d3f70897c
KiddMiguel <kidimbamiguel@gmail.com>
2025-02-03 21:56:09 +0100

    Front

--- src/vue/VueConnexion.java
+++ src/vue/VueConnexion.java
@@ -1,11 +1,9 @@
 package vue;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -18,6 +16,9 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
 
 import controleur.Alume;
 import controleur.Controleur;
@@ -29,43 +30,82 @@ public class VueConnexion extends JFrame implements ActionListener {
 	private JButton btAnnuler = new JButton("Annuler");
 	private JTextField txtEmail = new JTextField("a@gmail.com");
 	private JPasswordField txtMdp = new JPasswordField("123");
+	private JLabel txtInfo = new JLabel("Mot de passe oublier ?");
+	 private JLabel txtTitre = new JLabel("S'identifier", SwingConstants.CENTER);
+	 private JLabel txtMessage = new JLabel(
+			    "Connectez-vous pour gérer votre compte !"
+			  );
+	 private JPanel panelConnexion = new JPanel();
 	
 	private JPanel panelForm = new JPanel ();
+	
 	public VueConnexion() {
-		this.setTitle("Application ALUME_2025");
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setBounds(100, 100, 500, 600);
-		this.setLayout(null);
-		this.setResizable(false);
-		this.getContentPane().setBackground(Color.darkGray);
-		
-		//ajout de l'image logo
-		ImageIcon uneImage= new ImageIcon("src/images/alume_logo.png");
-		JLabel unLogo = new JLabel(uneImage);
-		unLogo.setBounds(100, 40, 300, 220);
-		this.add(unLogo);
-		
-		
-		//Construction du panel Formulaire
-		this.panelForm.setBounds(40, 280, 400, 240);
-		this.panelForm.setBackground(Color.darkGray);
-		this.panelForm.setLayout(new GridLayout(3,2)); //Matrice de 3 lignes et de 2 colonnes
-		this.panelForm.add(new JLabel("Email"));
-		this.panelForm.add(this.txtEmail);
-		this.panelForm.add(new JLabel("MDP"));
-		this.panelForm.add(this.txtMdp);
-		this.panelForm.add(this.btAnnuler);
-		this.panelForm.add(this.btSeConnecter);
-		
-		this.add(this.panelForm); //ajout du panel dans la fenetre
-		
-		//Rendre les boutonns ecoutables
-		this.btAnnuler.addActionListener(this);
-		this.btSeConnecter.addActionListener(this);
-		
-		this.setVisible(true);
-		
-    }
+	    this.setTitle("Neige et Soliel");
+	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    this.setResizable(false);
+	    this.setBounds(15, 20, 1500, 800);
+	    this.getContentPane().setBackground(new Color(255, 255, 255));
+	    this.setLayout(null);
+
+	    ImageIcon uneImage = new ImageIcon("src/Images/footer.png");
+	    JLabel unJLabel = new JLabel(uneImage);
+	    unJLabel.setBounds(0, 580, 1500, 200);
+	    this.add(unJLabel);
+
+	    ImageIcon uneImageLogo = new ImageIcon("src/Images/logo-noir.png");
+	    JLabel unJLabelLogo = new JLabel(uneImageLogo);
+	    unJLabelLogo.setBounds(0, 0, 250, 200);
+	    this.add(unJLabelLogo);
+
+	    //Pour notre titre
+	    this.txtTitre.setBounds(613, 120, 280, 200);
+	    this.txtTitre.setFont(new Font("Serif", Font.BOLD, 55));
+	    this.txtTitre.setForeground(new Color(59, 125, 221)); //blue
+	    this.add(this.txtTitre);
+
+	    //Pour notre titre
+	    this.txtMessage.setBounds(630, 170, 280, 200);
+	    this.txtMessage.setPreferredSize(new Dimension(250, 150));
+	    this.add(this.txtMessage);
+
+	    this.panelConnexion.setBounds(630, 300, 280, 200);
+	    this.panelConnexion.setBackground(new Color(255, 255, 255));
+	    this.panelConnexion.setLayout(new GridLayout(7, 0));
+
+	    //Pour L'input Email
+	    this.panelConnexion.add(new JLabel("Email"));
+	    this.panelConnexion.add(this.txtEmail);
+	    this.txtEmail.setBackground(new Color(59, 125, 221)); //Couleur de fond blue
+	    this.txtEmail.setForeground(new Color(255, 255, 255)); //couleur de text White
+	    Border LoweredBevelBorder = BorderFactory.createLoweredBevelBorder();
+	    this.txtEmail.setBorder(LoweredBevelBorder);
+
+	    //Pour l'input MDP
+	    this.panelConnexion.add(new JLabel("Mot de passe"));
+	    this.panelConnexion.add(this.txtMdp);
+	    this.txtMdp.setBackground(new Color(59, 125, 221)); //blue
+	    this.txtMdp.setForeground(new Color(255, 255, 255)); //couleur de text White
+	    this.txtMdp.setBorder(LoweredBevelBorder);
+
+	    //Pour le text MDP oublié
+	    this.panelConnexion.add(new JLabel(""));
+	    this.panelConnexion.add(this.txtInfo);
+	    this.txtMdp.setBackground(new Color(59, 125, 221)); //blue
+
+	    //Pour le bouton
+	    this.panelConnexion.add(this.btSeConnecter);
+	    this.btSeConnecter.setBackground(new Color(255, 200, 57));
+	    this.btSeConnecter.setBounds(540, 500, 250, 100);
+	    Border EtchedBorderLowered = BorderFactory.createEtchedBorder(
+	      EtchedBorder.LOWERED
+	    );
+	    this.btSeConnecter.setBorder(EtchedBorderLowered);
+	    this.btSeConnecter.addActionListener(this);
+
+	    this.add(this.panelConnexion);
+
+	    this.setVisible(true);
+	  }
 	
 
     @Override
